05205ccae1b76d4c83f91a56de33ded56b594dad
Rafael Magossi <rafael_mq2@hotmail.com>
2018-07-09 12:14:46 -0400

    Aula - Primeiro Exemplo Typescript

---
+++ Typescript/Primeiro Exemplo Typescript/app.ts
@@ -0,0 +1,13 @@
+let message: string = "Help me, Obi-wWan Kenobi. You're my only hope!" 
+console.log(message); 
+ 
+let episode: number = 4 
+episode = '4' 
+console.log("This is episode "+ 4); 
+episode = episode + 1 
+console.log("Next episode is "+episode); 
+ 
+let faviteDroid 
+faviteDroid = 'BB-8' 
+faviteDroid = 10 
+console.log("My Favorite droid is " + faviteDroid)
---
+++ Typescript/Primeiro Exemplo Typescript/dist/app.js
@@ -0,0 +1,11 @@
+var message = "Help me, Obi-wWan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+episode = '4';
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var faviteDroid;
+faviteDroid = 'BB-8';
+faviteDroid = 10;
+console.log("My Favorite droid is " + faviteDroid);
